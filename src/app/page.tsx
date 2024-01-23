b66c4f3ddad5c22270310c2bec79852aae959b6d
Anthony Cueva <hi.cuevantn@gmail.com>
2024-01-23 16:04:51 -0500

    feat: v0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,229 @@
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  AwardIcon,
+  ClockIcon,
+  MapPinIcon,
+  MoreHorizontalIcon,
+} from "lucide-react";
+import Link from "next/link";
+import { Card } from "@/components/ui/card";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import Image from "next/image";
+
+type User = {
+  id: string;
+  name: string;
+  username: string;
+  profilePicture?: string;
+};
+
+const users: User[] = [
+  {
+    id: "1",
+    name: "Anthony Cueva",
+    username: "cuevantn",
+    profilePicture: "https://avatars.githubusercontent.com/u/83598208?v=4",
+  },
+  {
+    id: "2",
+    name: "Guillermo Rauch",
+    username: "rauchg",
+    profilePicture: "https://avatars.githubusercontent.com/u/13041?v=4",
+  },
+  {
+    id: "3",
+    name: "Evan You",
+    username: "youyuxi",
+    profilePicture: "https://avatars.githubusercontent.com/u/499550?v=4",
+  },
+  {
+    id: "4",
+    name: "Tim Neutkens",
+    username: "timneutkens",
+    profilePicture: "https://avatars.githubusercontent.com/u/6324199?v=4",
+  },
+  {
+    id: "5",
+    name: "Rich Harris",
+    username: "Rich-Harris",
+    profilePicture: "https://avatars.githubusercontent.com/u/1162160?v=4",
+  },
+];
+
+type Event = {
+  id: string;
+  title: string;
+  description: string;
+  date: Date;
+  image: string;
+  location: string;
+  organizerId: string;
+  assistantsCount: number;
+  highlightedAssistantsIds: string[];
+};
+const events: Event[] = [
+  {
+    id: "1",
+    title: "Voley Playuyos",
+    description: "Voley Playa en Barranco",
+    date: new Date("2024-01-28T15:00:00.000Z"),
+    image:
+      "https://images.pexels.com/photos/2444852/pexels-photo-2444852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    location: "Playa los Yuyos, Barranco",
+    organizerId: "1",
+    assistantsCount: 7,
+    highlightedAssistantsIds: ["3", "2", "5"],
+  },
+  {
+    id: "2",
+    title: "Maratón Entel",
+    description: "Maratón Entel 2021",
+    date: new Date("2024-01-26T13:00:00.000Z"),
+    image:
+      "https://images.pexels.com/photos/2654902/pexels-photo-2654902.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    location: "Playa Costa Azul, Ventanilla",
+    organizerId: "2",
+    assistantsCount: 37,
+    highlightedAssistantsIds: ["3", "4", "1"],
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container flex w-screen h-screen space-x-8 p-4">
+      <aside className="flex-none w-72 bg-muted rounded p-4 space-y-4 flex flex-col rounded-xl">
+        <div className="flex-none flex items-center space-x-2">
+          <AwardIcon className="w-6 h-6" />
+          <span className="font-bold text-lg">Dezport</span>
         </div>
+
+        <div className="grow flex flex-col space-y-2">
+          <Button variant="outline">
+            <Link href="/">Inicio</Link>
+          </Button>
+          <Button variant="outline">
+            <Link href="/search">Buscar</Link>
+          </Button>
+        </div>
+
+        <div className="flex-none flex flex-col">
+          <Button variant="outline" className="h-min space-x-2 justify-start">
+            <Avatar className="flex-none">
+              <AvatarImage src="https://avatars.githubusercontent.com/u/83598208?v=4" />
+              <AvatarFallback>AC</AvatarFallback>
+            </Avatar>
+            <div className="grow flex flex-col -space-y-1 items-start">
+              <span className="">Anthony Cueva</span>
+              <span className="text-sm text-muted-foreground">@cuevantn</span>
+            </div>
+            <MoreHorizontalIcon className="flex-none w-6 h-6" />
+          </Button>
+        </div>
+      </aside>
+
+      <main className="grow">
+        <div className="space-y-4">
+          {events.map((event) => (
+            <EventCard {...event} />
+          ))}
+        </div>
+      </main>
+
+      <div className="flex-none w-96 border rounded-xl p-4">
+        <h2>secondary</h2>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
+
+const EventCard = (event: Event) => {
+  let host = users.find((user) => user.id === event.organizerId);
+  if (!host) return null;
+
+  let highlightedAssistants = users.filter((user) =>
+    event.highlightedAssistantsIds.includes(user.id)
+  );
+
+  return (
+    <Card key={event.id} className="overflow-hidden flex grid grid-cols-2">
+      <div className="">
+        <AspectRatio ratio={1 / 1}>
+          <Image
+            alt={"Image of " + event.title}
+            src={event.image}
+            fill
+            className="rounded-l-xl object-cover"
+          />
+        </AspectRatio>
+      </div>
+      <div className="flex flex-col p-4 space-y-2">
+        <div>
+          <h2 className="font-bold text-xl">
+            {event.title}
+            <span className="ml-2 font-normal">by</span>
+          </h2>
+          <div className="flex items-center space-x-2">
+            <Avatar className="flex-none">
+              <AvatarImage src={host.profilePicture} />
+              <AvatarFallback>{host.name[0]}</AvatarFallback>
+            </Avatar>
+            <div className="flex-grow flex flex-col -space-y-1">
+              <span className="text-sm">{host.name}</span>
+              <span className="text-sm text-muted-foreground">
+                @{host.username}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="grow space-y-2">
+          <div className="flex space-x-2 items-center">
+            <MapPinIcon className="w-4 h-4" />
+            <p className=""> {event.location}</p>
+          </div>
+
+          <div className="flex space-x-2 items-center">
+            <ClockIcon className="w-4 h-4" />
+            <p className="tabular-nums">
+              {new Intl.DateTimeFormat("es-PE", {
+                year: "numeric",
+                month: "numeric",
+                day: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+              }).format(event.date)}
+            </p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute flex -space-x-4">
+              {highlightedAssistants.map((user) => (
+                <Avatar className="w-7 h-7">
+                  <AvatarImage src={user.profilePicture} />
+                  <AvatarFallback>{user.name[0]}</AvatarFallback>
+                </Avatar>
+              ))}
+            </div>
+            <p className="text-sm text-muted-foreground pt-[2px] leading-6">
+              <span className="text-transparent">xxxxxxxxxx</span>
+              {highlightedAssistants[highlightedAssistants.length - 1].name +
+                " y " +
+                (event.assistantsCount - 1) +
+                " personas más asistirán"}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex space-x-4">
+          <Button variant="outline" className="flex-grow">
+            Asistir
+          </Button>
+          <Button variant="default" className="flex-grow">
+            Ver más
+          </Button>
+        </div>
+      </div>
+    </Card>
+  );
+};
